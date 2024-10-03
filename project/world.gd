57b57830148804e9263937411185c1800016591b
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-10-03 15:07:31 -0400

    Add juice to the score by lerping up

--- project/world.gd
+++ project/world.gd
@@ -1,13 +1,15 @@
 extends Node3D
 
 @export var power := 10.0
-var score := 0:
-	set(value):
-		score = value
-		%ScoreLabel.text = "Score: %d" % score
+var score := 0.0
 
+var _displayed_score := 0.0:
+	set(value):
+		_displayed_score = value
+		%ScoreLabel.text = "Score: %d" % ceilf(_displayed_score)
 
 func _physics_process(_delta: float) -> void:
+	_displayed_score = lerp(_displayed_score, score, 0.2)
 	if Input.is_action_just_pressed("launch"):
 		$Bird.apply_impulse(Vector3.FORWARD * power)
 
